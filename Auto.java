dd2ff3e5841991fe74472e9ad3e2ccff73302fc3
Lollykonkon <52105849+Lollykonkon@users.noreply.github.com>
2022-02-07 08:16:22 +0900

    Create Auto.java

---
+++ Auto.java
@@ -0,0 +1,36 @@
+package Primeri;
+import java.util.Scanner; //подключаем класс для ввода данных с клавиатуры в консоли
+
+public class Auto {
+    private String firm; //создаем закрытый член нашего класса с названием фирмы автомобиля
+    private String num;
+    private int maxSpeed; // закрытый член класса, содержащий максимальную скорость
+    public void setFirm(String firma){ //открытая функция (метод класса) для задания
+        firm=firma; //значения фирмы автомобиля
+    }
+    public void setNumber(String number) { //открытая функция (метод класса) для задания
+        num = number; //значения госномера
+    }
+    public void setMaxSpeed(int speed){ //открытая функция (метод класса) для задания
+        maxSpeed=speed; //значения максимальной скорости автомобиля
+    }
+    public int getMaxSpeed(){ //открытая функция (метод класса) для вывода значения
+        return maxSpeed; //максимальной скорости
+    }
+    public String getFirm(){ //открытая функция (метод класса) для вывода значения
+        return firm; //заданной фирмы
+    }
+    public String getNumber(){ //открытая функция (метод класса) для вывода значения
+        return num; //заданной фирмы
+    }
+    public Auto(){ // конструктор класса (без параметров)
+        firm="Без названия";
+        maxSpeed=0;
+        num="a000aa";
+    }
+    public Auto(String firma, String number, int speed){ //конструктор класса (с параметрами)
+        firm=firma;
+        maxSpeed=speed;
+        num=number;
+    }
+}
